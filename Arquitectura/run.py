283ac2e6b3b20b459f11a9dbb5cfbdd548f3892c
Pedrobravo-notfound <juvenesxd@gmail.com>
2026-05-14 22:53:03 -0500

    Alinea CI y Render con despliegue blueprint

--- Arquitectura/run.py
+++ Arquitectura/run.py
@@ -6,7 +6,7 @@ app = create_app()
 if __name__ == '__main__':
     debug_mode = os.environ.get('FLASK_DEBUG', '0') == '1'
     app.run(
-        host='0.0.0.0',
+        host=os.environ.get('HOST', '127.0.0.1'),
         port=int(os.environ.get('PORT', 5000)),
         debug=debug_mode,
         use_reloader=debug_mode,
